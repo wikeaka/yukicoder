371069977b9c50176107d159c4a2049b6392ba9a
Keita Wakabayashi <wikeaka@gmail.com>
2016-08-23 18:55:54 +0900

    Removed loop

--- 46-first-n-step.cpp
+++ 46-first-n-step.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main() {
   int a, b;
   cin >> a >> b;
+
   int step = 0;
-  while (a * step < b) {
-    ++step;
-  }
+  if (b % a == 0)
+    step = b / a;
+  else
+    step = b / a + 1;
+
   cout << step << endl;
   return 0;
 }
